ce0d43272f184e7f02b9474c7e9888abcd1fa77d
Matt Alvarado <matimat2010@gmail.com>
2020-06-10 07:58:56 -0500

    Updates

---
+++ precache-manifest.53bb0b5673152e384ed9ef8405bd5da3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "563efd08a36e531e6da74063daa17d1e",
+    "url": "/weather-react/index.html"
+  },
+  {
+    "revision": "0b0f8bc376ab15317b9a",
+    "url": "/weather-react/static/css/main.b0d5475d.chunk.css"
+  },
+  {
+    "revision": "1a98a69ecb58d8af4949",
+    "url": "/weather-react/static/js/2.2c8de82b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-react/static/js/2.2c8de82b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b0f8bc376ab15317b9a",
+    "url": "/weather-react/static/js/main.f19c077b.chunk.js"
+  },
+  {
+    "revision": "7b66ce6d95abc4d082ff",
+    "url": "/weather-react/static/js/runtime-main.266d8c40.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/weather-react/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/weather-react/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
